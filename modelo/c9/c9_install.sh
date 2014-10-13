3f7eec52c5c3f5956075ec723c8e25e51be13af6
senapk <sena.ufc@gmail.com>
2014-10-13 18:51:25 +0000

    c9

--- modelo/c9/c9_install.sh
+++ modelo/c9/c9_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #apos clonar o repositorio no c9, rode esse script para habilitar o c++11, o motor e os scripts
-sudo cp ./C++\ (simple).run ~/workspace/.c9/runners
+sudo cp ./C++\ \(simple\).run ~/workspace/.c9/runners
 sudo cp ./../../motor/cmotor.h /usr/local/include/
 sudo cp ./../../motor/motor.h /usr/local/include/
 sudo cp ./../scripts/kcheck.py /usr/local/bin
