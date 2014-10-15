9cfc7397b9de791a7e809b33571fc70fcfce8c64
ticyamora <ticyamora@gmail.com>
2014-10-15 13:59:49 -0300

    Update 2_big_div_11.a.cpp
    Melhorando a dica colocando a palavra índice.

--- arcade/07_vetores/questoes/2_big_div_11.a.cpp
+++ arcade/07_vetores/questoes/2_big_div_11.a.cpp
@@ -16,9 +16,6 @@ using namespace std;
 //operações com estes números? Começe com uma operação simples, faça uma função que
 //receba um número gigante com até 1000 casas e retorne true se ele é divisível
 //por 11. Existem formas para se determinar essa divisibilidade. Pesquise um pouco.
-//Dica: Um número é divisível por 11 se a soma dos algarísmos de posição par
-//subtraída da soma dos algarísmos de posição ímpar resultar 0 ou um número
-//divisível por 11.
 
 bool eh_divisivel_por_11(string num){
     return false;
@@ -39,3 +36,10 @@ int main(){
     cerr << endl << "#end";
     return(0);
 }
+
+//Existe uma relação entre as somas dos algarismos índice par e índice ímpar.
+//Se você somar todos os algarismos índice par terá sum_par;
+//Se você somar todos os algarismos índice impar terá sum_impar;
+
+//Se (si_impar - si_par) for divisível por 11, então o número todo é divisível.
+
